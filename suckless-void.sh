222a4a73d015f0bec1a650d1cb1b806f8a08977d
Miguel Reis <miguel_reis08@protonmail.com>
2023-02-26 10:23:21 -0300

    Update script

--- suckless-void.sh
+++ suckless-void.sh
@@ -9,7 +9,7 @@ sudo su <<- EOF
   # Variables
   user="$user"
   home="$home"
-  dependencies="base-devel tcc xorg libXft-devel libX11-devel libXinerama-devel xf86-video-intel xinit xrdb xrandr unclutter xclip redshift xbacklight alsa-utils vim tmux lynx pass pcmanfm"
+  dependencies="base-devel tcc xorg-minimal xorg-fonts xf86-video-intel libX11-devel libXft-devel libXinerama-devel xinit xrdb xrandr unclutter xclip redshift xbacklight alsa-utils vim tmux lynx pass pcmanfm"
   tools="dwm dmenu st slstatus"
 
   # Upgrade system and install dependencies
